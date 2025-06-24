b818731779be2dac1ee62761a1050ac4b8466091
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-06-24 17:48:59 +0500

    Book Removed

--- src/HirefyAI.Api/obj/Debug/net8.0/HirefyAI.Api.AssemblyInfo.cs
+++ src/HirefyAI.Api/obj/Debug/net8.0/HirefyAI.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HirefyAI.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd2ddebf1108864f64d7be3f63b76052c739ec10")]
 [assembly: System.Reflection.AssemblyProductAttribute("HirefyAI.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HirefyAI.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/HirefyAI.Application/obj/Debug/net8.0/HirefyAI.Application.AssemblyInfo.cs
+++ src/HirefyAI.Application/obj/Debug/net8.0/HirefyAI.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HirefyAI.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd2ddebf1108864f64d7be3f63b76052c739ec10")]
 [assembly: System.Reflection.AssemblyProductAttribute("HirefyAI.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HirefyAI.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
