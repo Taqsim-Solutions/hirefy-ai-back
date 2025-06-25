8e2e7e2a2b25f8e62eb23b7e589ab8f4f105ab2a
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-06-25 17:54:52 +0500

    Email bo'yicha user bilan shlash

---
+++ src/HirefyAI.Infrastructure/Helpers/UserHelper.cs
@@ -0,0 +1,35 @@
+﻿using HirefyAI.Domain.Entities;
+
+namespace HirefyAI.Infrastructure.Helpers
+{
+    public class UserHelper
+    {
+        private readonly HirefyAIDb _hirefyAIDb;
+
+        public UserHelper(HirefyAIDb hirefyAIDb)
+        {
+            _hirefyAIDb = hirefyAIDb;
+        }
+
+        public async Task<int> UserId(string email)
+        {
+            var user = _hirefyAIDb.Users.FirstOrDefault(u => u.Email == email);
+            if (user != null)
+            {
+                return user.Id;
+            }
+
+            user = new User()
+            {
+                Email = email,
+                FirstName = "New",
+                LastName = "User"
+            };
+
+            var entry = await _hirefyAIDb.Users.AddAsync(user);
+            await _hirefyAIDb.SaveChangesAsync();
+
+            return entry.Entity.Id;
+        }
+    }
+}
